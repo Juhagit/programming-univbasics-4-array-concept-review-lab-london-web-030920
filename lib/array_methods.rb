f8ec700635cd51555e70658652f1998007e988de
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-23 05:16:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       return count
     end
-    count += 0 
+    count += 1  
     end
 
 def find_max_value(array)
